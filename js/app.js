23007215e66ae6013eff8ed405d6d8a15382f62d
Ericesposito <eespo93@optonline.net>
2024-02-25 15:20:10 -0500

    Building out static html skeleton

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', (e) => {
   console.log('Document is fully loaded and parsed');
+
+  const interfaceWindow = document.getElementById('interface-window');
+
+  interfaceWindow.append;
+
+  for (let i = 0; i < 10; i++) {}
   document.addEventListener('click', (e) => {
     console.log('page clicked');
   });
